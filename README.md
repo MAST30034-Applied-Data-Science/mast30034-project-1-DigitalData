8c7c926f7d8840b629afa4fb3f58459e7a4ed226
xtravers <legoxavierlocketravers@gmail.com>
2022-07-31 01:57:20 +1000

    preprocessing: just needs a commenting run
    most of the preprocessing is done.
    - need to figure out difference between data analysis and modelling I guess.

--- README.md
+++ README.md
@@ -21,12 +21,21 @@ Remember, we will be reading through and running your code, so it is in _your be
 ## README example
 This is an example `README.md` for students to use. **Please change this to your requirements**.
 
-**Research Goal:** My research goal is tip analysis for credit card payments
+**Research Goal:** To determine the effect that the COVID pandemic has had on the frequency of use of taxi services in New York.
 
-**Timeline:** The timeline for the research area is 2018 - 2021.
+**Timeline:** The timeline for the research area is starting July 2019 and ending June 2021.
 
-To run the pipeline, please visit the `scripts` directory and run the files in order:
-1. `download.py`: This downloads the raw data into the `data/raw` directory.
-2. `preprocess.ipynb`: This notebook details all preprocessing steps and outputs it to the `data/curated` directory.
+**Pipeline:**
+*Run all the scripts from the repository's root directory (do not `cd` into the `scripts` folder).*
+
+1. `download.py`: This downloads the raw data into the `data/raw` directory. 
+Run ```python3 ./scripts/download.py```.
+2. `preprocessing_part_1_conversion.ipynb`: Converts any `.csv` datasets into the `.parquet` format.
+3. `preprocessing_part_2_cleaning.ipynb`: Cleans the dataset (removes rows containing `null` and negative values where necessary).
+4. `preprocessing_part_3_aggregation.ipynb`: This groups the datasets by month, year, taxi type, shared-ride status (which is a passenger count for green/yellow taxis and a boolean value for high frequency taxi services).
+5. ``
+6. ``
+
+<!-- 2. `preprocess.ipynb`: This notebook details all preprocessing steps and outputs it to the `data/curated` directory.
 3. `analysis.ipynb`: This notebook is used to conduct analysis on the curated data.
-4. `model.py` and `model_analysis.ipynb`: The script is used to run the model from CLI and the notebook is used for analysing and discussing the model.
+4. `model.py` and `model_analysis.ipynb`: The script is used to run the model from CLI and the notebook is used for analysing and discussing the model. -->
--- scripts/download.py
+++ scripts/download.py
@@ -4,31 +4,77 @@ Created with reference to the `Python_PreReq_Notebook.ipynb` provided.
 Xavier Travers
 1178369
 '''
+import itertools
 from urllib.request import urlretrieve
 import os
-
-# for debug prints
-DEBUGGING = False
+import time
 
 # output directory
 out_dir_rel = './data/raw/'
 
-# make it if nonexistant
+# make required paths if nonexistant
 if not os.path.exists(out_dir_rel):
     os.makedirs(out_dir_rel)
 
-
 # list of urls to dl datasets from
-dl_dict = { # basic defaults
-    "tlc/taxi_zones.csv": "https://data.cityofnewyork.us/api/views/755u-8jsi/rows.csv?accessType=DOWNLOAD",
+dl_dict = { # the easy ones
+    'tlc_zones/zones.csv': 'https://data.cityofnewyork.us/api/views/755u-8jsi/rows.csv?accessType=DOWNLOAD',
+    'covid/cases-by-day.csv': 'https://raw.githubusercontent.com/nychealth/coronavirus-data/master/trends/data-by-day.csv'
 }
 
 # add the tlc data over the 6 months nov-2019 -> apr-2020 (when COVID hits)
-URL_TLC_TEMPLATE = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_"
+URL_TLC_TEMPLATE = 'https://d37ci6vzurychx.cloudfront.net/trip-data/'
+TLC_NAMES = ['yellow', 'green', 'fhvhv']
+TLC_DATES = { # the necessary dates for analysis
+    2019: range(6,13),
+    2020: range(1,13),
+    2021: range(1,7),
+}
 
+# iterate through tlc types to download
+for name in TLC_NAMES:
 
+    # iterate through the years and specific months necessary for analysis
+    for year, months in TLC_DATES.items():
 
-# perform url retrieval and dl
-for filename, url in dl_dict:
-    out_dir = f"{out_dir_rel}{filename}"
-    urlretrieve(url, out_dir)
+        # iterate over months
+        for m in months:
+            # convert month to proper format 
+            month = str(m).zfill(2)
+
+            # add this file to the download dictionary
+            dl_dict[
+                f'tlc/{name}/{year}-{month}.parquet'
+            ] = f'{URL_TLC_TEMPLATE}{name}_tripdata_{year}-{month}.parquet'
+
+# start the time measurement
+start = time.time()
+start_dl = 0
+
+# count number of downloaded files
+num_downloaded = 0
+
+# perform download the files according to the dict
+for filename, url in dl_dict.items():
+    out_dir = f'{out_dir_rel}{filename}'
+
+    # skip if it's already downloaded
+    if os.path.exists(out_dir): continue
+
+    # create the needed dir for this dl file if necessary
+    if not os.path.exists(os.path.dirname(out_dir)):
+        os.makedirs(os.path.dirname(out_dir))
+
+    # start the time for this download
+    start_dl = time.time()
+
+    # download it
+    print(f'DOWNLOADING \"{filename}\" FROM \"{url}\"')
+    try:
+        urlretrieve(url, out_dir)
+        print(f'\tSUCCESS! TOOK {(time.time() - start_dl):.2f}s')
+    except: # skip if the link is forbidden (example: for fhvhv in 2018)
+        print(f'\tDOWNLOAD ERROR! TOOK {(time.time() - start_dl):.2f}s')
+        continue
+
+print(f'\nDONE IN {(time.time() - start):.2f}s')
